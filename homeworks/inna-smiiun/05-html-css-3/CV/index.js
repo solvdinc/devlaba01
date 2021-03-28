88cb1dcdbd007a9423dbe0bd4f3273da9045e7e9
innasmiiun <innasmiyun@gmail.com>
2021-03-28 13:13:08 +0300

    Edited script file.

--- homeworks/inna-smiiun/05-html-css-3/CV/index.js
+++ homeworks/inna-smiiun/05-html-css-3/CV/index.js
@@ -1,21 +1,20 @@
-window.addEventListener("load", (event) => {
-  let contacts = document.getElementById("contacts");
-  let buttonContacts = document.getElementById("button__contacts");
-  let button = document.getElementById("btn-top");
-
-  button.addEventListener("click", onTop);
-  buttonContacts.addEventListener("click", showContacts);
+window.addEventListener('load', () => {
+  let contacts = document.getElementById('contacts');
+  let buttonContacts = document.getElementById('button__contacts');
+  let button = document.getElementById('btn-top');
 
   function onTop() {
     document.documentElement.scrollTop = 0;
   }
 
   function showContacts() {
-    if (contacts.classList.contains("show")) {
-      contacts.classList.remove("show");
+    if (contacts.classList.contains('show')) {
+      contacts.classList.remove('show');
     } else {
-      contacts.classList.add("show");
+      contacts.classList.add('show');
     }
   }
-});
 
+  button.addEventListener('click', onTop);
+  buttonContacts.addEventListener('click', showContacts);
+});
