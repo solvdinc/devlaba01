4446c5611299e3a67a3db5164b80cfaf982c8fce
ilya07092000 <ischenko2012@gmail.com>
2021-04-01 20:48:43 +0300

    fix: whitespaces

--- homeworks/ilya-ischenko/06-js-basic/script.js
+++ homeworks/ilya-ischenko/06-js-basic/script.js
@@ -1,27 +1,27 @@
 // Opposite number - 1
 const oppositeNumber = function (number) {
-	return -number;
+  return -number;
 };
 console.log(oppositeNumber(1));
 
 // Basic Mathematical Operations - 2
 const basicOp = function (operation, value1, value2) {
-    return eval(`${value1} ${operation} ${value2}`);
+  return eval(`${value1} ${operation} ${value2}`);
 };
 console.log(basicOp('+', 2, 2));
 // DUE TO RULE 6.4 Airbnb I SUGGEST ANOTHER SOLUTION
 const basicOp1 = function (operation, value1, value2) {
-    switch (operation) {
-        case '+':
-            return value1 + value2;
-        case '-': 
-            return value1 - value2;
-        case '*': 
-            return value1 * value2;
-        case '/':
-            return value1 / value2;
-        default:
-            return 0;
-    };
+  switch (operation) {
+    case '+':
+      return value1 + value2;
+    case '-': 
+      return value1 - value2;
+    case '*': 
+      return value1 * value2;
+    case '/':
+      return value1 / value2;
+    default:
+      return 0;
+  };
 };
 console.log(basicOp1('+', 2, 5));
