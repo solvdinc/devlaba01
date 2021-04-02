6ed2d9ac81a7d131ade08d8b532e71d8a8fc8bcd
ilya07092000 <ischenko2012@gmail.com>
2021-04-02 12:13:20 +0300

    improve code style

--- homeworks/ilya-ischenko/06-js-basic/script.js
+++ homeworks/ilya-ischenko/06-js-basic/script.js
@@ -1,3 +1,5 @@
+/* eslint-disable func-names */
+/* eslint-disable no-console */
 // Opposite number - 1
 const oppositeNumber = function (number) {
   return -number;
@@ -6,6 +8,7 @@ console.log(oppositeNumber(1));
 
 // Basic Mathematical Operations - 2
 const basicOp = function (operation, value1, value2) {
+  // eslint-disable-next-line no-eval
   return eval(`${value1} ${operation} ${value2}`);
 };
 console.log(basicOp('+', 2, 2));
@@ -14,20 +17,20 @@ const basicOp1 = function (operation, value1, value2) {
   switch (operation) {
     case '+':
       return value1 + value2;
-    case '-': 
+    case '-':
       return value1 - value2;
-    case '*': 
+    case '*':
       return value1 * value2;
     case '/':
       return value1 / value2;
     default:
       return 0;
-  };
+  }
 };
 console.log(basicOp1('+', 2, 5));
 
 // Printing Array elements with Comma delimiters - 3
 const printArray = function (array) {
-    return array.join(',');
+  return array.join(',');
 };
-console.log(printArray(["h","o","l","a"]));
+console.log(printArray(['h', 'o', 'l', 'a']));
