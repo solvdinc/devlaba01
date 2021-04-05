9afe94028a1d1ca98732f3fabb552cc8555b848d
ilya07092000 <ischenko2012@gmail.com>
2021-04-05 19:42:33 +0300

    remove space bafore func args

--- homeworks/ilya-ischenko/06-js-basic/script.js
+++ homeworks/ilya-ischenko/06-js-basic/script.js
@@ -1,19 +1,19 @@
 /* eslint-disable func-names */
 /* eslint-disable no-console */
 // 1 - http://www.codewars.com/kata/opposite-number
-const oppositeNumber = function (number) {
+const oppositeNumber = function(number) {
   return -number;
 };
 console.log(oppositeNumber(1));
 
 // 2 - http://www.codewars.com/kata/basic-mathematical-operations
-const basicOp = function (operation, value1, value2) {
+const basicOp = function(operation, value1, value2) {
   // eslint-disable-next-line no-eval
   return eval(`${value1} ${operation} ${value2}`);
 };
 console.log(basicOp('+', 2, 2));
 // DUE TO RULE 6.4 Airbnb I SUGGEST ANOTHER SOLUTION
-const basicOp1 = function (operation, value1, value2) {
+const basicOp1 = function(operation, value1, value2) {
   switch (operation) {
     case '+':
       return value1 + value2;
@@ -30,13 +30,13 @@ const basicOp1 = function (operation, value1, value2) {
 console.log(basicOp1('+', 2, 5));
 
 // 3 - http://www.codewars.com/kata/printing-array-elements-with-comma-delimiters
-const printArray = function (array) {
+const printArray = function(array) {
   return array.join(',');
 };
 console.log(printArray(['h', 'o', 'l', 'a']));
 
 // 4 -http://www.codewars.com/kata/transportation-on-vacation
-const rentalCarCost = function (d) {
+const rentalCarCost = function(d) {
   let discount = 0;
   const rentalCost = 40;
 
@@ -51,61 +51,61 @@ const rentalCarCost = function (d) {
 console.log(rentalCarCost(10));
 
 // 5 - http://www.codewars.com/kata/calculating-with-functions
-const zero = function (f) {
+const zero = function(f) {
   return f ? f(0) : 0;
 };
-const one = function (f) {
+const one = function(f) {
   return f ? f(1) : 1;
 };
-const two = function (f) {
+const two = function(f) {
   return f ? f(2) : 2;
 };
-const three = function (f) {
+const three = function(f) {
   return f ? f(3) : 3;
 };
-const four = function (f) {
+const four = function(f) {
   return f ? f(4) : 4;
 };
-const five = function (f) {
+const five = function(f) {
   return f ? f(5) : 5;
 };
-const six = function (f) {
+const six = function(f) {
   return f ? f(6) : 6;
 };
-const seven = function (f) {
+const seven = function(f) {
   return f ? f(7) : 7;
 };
-const eight = function (f) {
+const eight = function(f) {
   return f ? f(8) : 8;
 };
-const nine = function (f) {
+const nine = function(f) {
   return f ? f(9) : 9;
 };
 
-const plus = function (n1) {
-  return function (n2) {
+const plus = function(n1) {
+  return function(n2) {
     return n2 + n1;
   };
 };
-const minus = function (n1) {
-  return function (n2) {
+const minus = function(n1) {
+  return function(n2) {
     return n2 - n1;
   };
 };
-const times = function (n1) {
-  return function (n2) {
+const times = function(n1) {
+  return function(n2) {
     return n2 * n1;
   };
 };
-const dividedBy = function (n1) {
-  return function (n2) {
+const dividedBy = function(n1) {
+  return function(n2) {
     return Math.floor(n2 / n1);
   };
 };
 console.log(five(plus(six())));
 
 // 6 - http://www.codewars.com/kata/get-the-middle-character
-const getMiddle = function (s) {
+const getMiddle = function(s) {
   const even = (num) => !(num % 2);
   const middle = Math.round(s.length / 2);
 
@@ -140,14 +140,14 @@ function partitionOn (pred, items) {
 }
 
 // 8 - http://www.codewars.com/kata/word-count
-const countWords = function (str) {
+const countWords = function(str) {
   const letterStr = str.match(/[^\s]+/g) || [];
   return letterStr.length;
 };
 console.log(countWords('I am string'));
 
 // 9 - https://www.codewars.com/kata/find-the-odd-int/
-const findOdd = function (A) {
+const findOdd = function(A) {
   let tempArr = [];
 
   for (let i = 0; i < A.length; i++) {
@@ -163,7 +163,7 @@ const findOdd = function (A) {
 console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
 
 // 10 - https://www.codewars.com/kata/find-the-parity-outlier
-const findOutlier = function (integers) {
+const findOutlier = function(integers) {
   const oddArr = [];
   const evenArr = [];
 
@@ -180,20 +180,20 @@ const findOutlier = function (integers) {
 console.log(findOutlier([70348169, -21048669, -37158567, -72614069, -15491459, 160941471, 8]));
 
 // 11 - https://www.codewars.com/kata/zipwith
-const zipWith = function (fn, a0, a1) {
+const zipWith = function(fn, a0, a1) {
   let minArr = a0.length < a1.length ? a0 : a1;
 
   return minArr.map((item, index) => fn(a0[index], a1[index]));
 };
 
 // 12 - https://www.codewars.com/kata/filter-the-number
-const FilterString = function (value) {
+const FilterString = function(value) {
   return +value.match(/\d/g).join('');
 };
 console.log(FilterString('12fd'));
 
 // 13 https://www.codewars.com/kata/n-th-fibonacci
-const nthFibo = function (n) {
+const nthFibo = function(n) {
   const fibArr = [0, 1];
 
   for (let i = 0; i < n; i++) {
@@ -205,7 +205,7 @@ const nthFibo = function (n) {
 console.log(nthFibo(4));
 
 // 14 - https://www.codewars.com/kata/cat-and-mouse-2d-version/
-const catMouse = function (map, moves) {
+const catMouse = function(map, moves) {
   const field = [];
   const maxMoves = moves;
   let gameMap = map;
@@ -242,7 +242,7 @@ const catMouse = function (map, moves) {
 console.log(catMouse('..C...... ......... ........m', 5));
 
 // 15 - https://www.codewars.com/kata/duplicate-encoder
-const duplicateEncode = function (word) {
+const duplicateEncode = function(word) {
   const wordArr = word.toLowerCase().split('');
   const result = [];
 
@@ -259,7 +259,7 @@ const duplicateEncode = function (word) {
 console.log(duplicateEncode('Success'));
 
 // 16 - https://www.codewars.com/kata/5693239fb761dc8670000001
-const findAdditiveNumbers = function (num) {
+const findAdditiveNumbers = function(num) {
   const numArr = num.split('');
   let firstNum = numArr[0];
   let secondNum = numArr[1];
@@ -304,7 +304,7 @@ const findAdditiveNumbers = function (num) {
 console.log(findAdditiveNumbers('101'));
 
 // 17 - https://www.codewars.com/kata/576757b1df89ecf5bd00073b
-const towerBuilder = function (nFloors) {
+const towerBuilder = function(nFloors) {
   const arr = [];
   const floorLength = nFloors * 2 - 1;
   let floor = [];
@@ -332,7 +332,7 @@ const towerBuilder = function (nFloors) {
 console.log(towerBuilder(2));
 
 // 18 - https://www.codewars.com/kata/58f5c63f1e26ecda7e000029
-const wave = function (str) {
+const wave = function(str) {
   const resultArr = [];
   let cloneStr;
 
@@ -348,7 +348,7 @@ const wave = function (str) {
 console.log(wave('two words'));
 
 // 19 - https://www.codewars.com/kata/59d398bb86a6fdf100000031
-const stringBreakers = function (n, string) {
+const stringBreakers = function(n, string) {
   const str = string.replace(/\s/g, '');
   const result = [];
 
@@ -364,7 +364,7 @@ const stringBreakers = function (n, string) {
 console.log(stringBreakers(5, 'This is an example string'));
 
 // 20 - https://www.codewars.com/kata/514a024011ea4fb54200004b
-const domainName = function (url) {
+const domainName = function(url) {
   let copyUrl = url;
 
   copyUrl = copyUrl.replace('http://', '');
