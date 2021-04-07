6d0aa9433831d5c048091a229de189f7861600fa
Freshaku <52348428+Freshaku@users.noreply.github.com>
2021-04-08 00:51:39 +0300

    fix codestyle

--- homeworks/karina-akulenok/06-js-basic/script.js
+++ homeworks/karina-akulenok/06-js-basic/script.js
@@ -1,76 +1,81 @@
 // task 1 http://www.codewars.com/kata/opposite-number
 function opposite(number) {
-  return(number = -number);
+  const oppositeNum = -number;
+  return oppositeNum;
 }
-  
+
 // task 2 http://www.codewars.com/kata/basic-mathematical-operations
-function basicOp(operation, value1, value2){
-  if(operation === "+") {
-      return (value1 + value2);
-  } else if(operation === "-") {
-      return (value1 - value2);
-  } else if(operation === "*") {
-      return (value1 * value2);
-  } else if(operation === "/") {
-      return (value1 / value2);
+function basicOp(operation, value1, value2) {
+  if (operation === '+') {
+    return (value1 + value2);
+  }
+  if (operation === '-') {
+    return (value1 - value2);
+  }
+  if (operation === '*') {
+    return (value1 * value2);
+  }
+  if (operation === '/') {
+    return (value1 / value2);
   }
 }
 
 // task 3 http://www.codewars.com/kata/printing-array-elements-with-comma-delimiters
-function printArray(array){
+function printArray(array) {
   return array.join(',');
 }
 
 // task 4 http://www.codewars.com/kata/transportation-on-vacation
 function rentalCarCost(days) {
-  let coast = 40 * days;
-  if(days >= 7) {
-    return (coast = coast - 50);
+  const coast = 40 * days;
+  let newCoast;
+  if (days >= 7) {
+    newCoast = coast - 50;
+    return newCoast;
   }
-  if(days >= 3) {
-    return (coast = coast - 20);
-  } else {
-    return coast;
+  if (days >= 3) {
+    newCoast = coast - 20;
+    return newCoast;
   }
+  return coast;
 }
 
 // task 5 http://www.codewars.com/kata/calculating-with-functions
-function value (num, operation) { return !operation ? num : operation(num)}
-function zero (operation) { return value(0, operation) }
-function one (operation) { return value(1, operation) }
-function two (operation) { return value(2, operation) }
-function three (operation) { return value(3, operation) }
-function four (operation) { return value(4, operation) }
-function five (operation) { return value(5, operation) }
-function six (operation) { return value(6, operation) }
-function seven (operation) { return value(7, operation) }
-function eight (operation) { return value(8, operation) }
-function nine (operation) { return value(9, operation) }
+function value(num, operation) { return !operation ? num : operation(num); }
+function zero(operation) { return value(0, operation); }
+function one(operation) { return value(1, operation); }
+function two(operation) { return value(2, operation); }
+function three(operation) { return value(3, operation); }
+function four(operation) { return value(4, operation); }
+function five(operation) { return value(5, operation); }
+function six(operation) { return value(6, operation); }
+function seven(operation) { return value(7, operation); }
+function eight(operation) { return value(8, operation); }
+function nine(operation) { return value(9, operation); }
 
-function plus(a) { return function(b){return (b + a)} }
-function minus(a) { return function(b){return (b - a)} }
-function times(a) { return function(b){return (b * a)} }
-function dividedBy(a) { return function(b){return Math.floor(b / a)} }
+function plus(a) { return function(b) { return (b + a); }; }
+function minus(a) { return function(b) { return (b - a); }; }
+function times(a) { return function(b) { return (b * a); }; }
+function dividedBy(a) { return function(b) { return Math.floor(b / a); }; }
 
 // task 6 http://www.codewars.com/kata/get-the-middle-character
 function getMiddle(s) {
   let center;
-  for(let i = 0; i <= s.length; i++) {
+  for (let i = 0; i <= s.length; i++) {
     if ((s.length % 2) === 0) {
       center = s.length / 2 - 1;
       return (s.substring(center, center + 2));
-    } else {
-      center = s.length / 2;
-      return (s.substring(center, center + 1));
     }
+    center = s.length / 2;
+    return (s.substring(center, center + 1));
   }
 }
 
 // task 7 http://www.codewars.com/kata/partition-on
 function partitionOn(pred, items) {
-  let pTrue = [];
-  let pFalse = [];
-  items.forEach( function(value) {
+  const pTrue = [];
+  const pFalse = [];
+  items.forEach(function(value) {
     (pred(value)) ? pTrue.push(value) : pFalse.push(value)
   });
   items.length = 0;
@@ -80,52 +85,51 @@ function partitionOn(pred, items) {
 
 // task 8 http://www.codewars.com/kata/word-count
 function countWords(str) {
-  let matches = str.match(/[\w\d\’\'-]+/gi);
+  const matches = str.match(/[\w\d\’\'-]+/gi);
   return matches ? matches.length : 0;
 }
 
 // task 9 https://www.codewars.com/kata/find-the-odd-int/
 function findOdd(arr) {
-  for(let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     let count = 0;
-    for(let j = 0; j < arr.length; j++) {
+    for (let j = 0; j < arr.length; j++) {
       if (arr[i] === arr[j]) { count++; }
     }
-    if (count % 2 != 0) { return A[i]; }
+    if (count % 2 !== 0) { return arr[i]; }
   }
   return 0;
 }
 
 // task 10 https://www.codewars.com/kata/find-the-parity-outlier
 function findOutlier(integers) {
-  let odd = [];
-  let even = [];
-  for(let i = 0; i <= integers.length; i++) {
-    if(integers[i] % 2 === 0) {
+  const odd = [];
+  const even = [];
+  for (let i = 0; i <= integers.length; i++) {
+    if (integers[i] % 2 === 0) {
       odd.push(integers[i]);
-    }else {
-      even.push(integers[i]);
     }
+    even.push(integers[i]);
   }
   return odd.length === 1 ? odd[0] : even[0];
 }
 
 // task 11 https://www.codewars.com/kata/zipwith
-function zipWith(fn,a0,a1) {
-  let result = [];
+function zipWith(fn, a0, a1) {
+  const result = [];
   const minArr = Math.min(a0.length, a1.length);
-  for (let i=0; i < minArr; i++) {
+  for (let i = 0; i < minArr; i++) {
     result.push(fn(a0[i], a1[i]));
   }
   return result;
 }
 
 // task 12 https://www.codewars.com/kata/filter-the-number
-var FilterString = function(value) {
-  let nValue = value.split('').filter(el => isNaN(el) === false).join('');
-  let result = parseInt(nValue);
+const FilterString = function(value) {
+  const nValue = value.split('').filter((el) => isNaN(el) === false).join('');
+  const result = parseInt(nValue);
   return result;
-}
+};
 
 // task 13 https://www.codewars.com/kata/n-th-fibonacci
 function nthFibo(n) {
@@ -133,7 +137,7 @@ function nthFibo(n) {
   let b = 1;
   if (n === 1) return 0;
   for (let i = 3; i <= n; i++) {
-    let c = a + b;
+    const c = a + b;
     a = b;
     b = c;
   }
@@ -141,30 +145,30 @@ function nthFibo(n) {
 }
 
 // task 14 https://www.codewars.com/kata/cat-and-mouse-2d-version/
-function catMouse(map,moves) {
-  map = map.split("\n");
-  let locationC = map.map((s) => s.indexOf("C"));
-  let locationM = map.map((s) => s.indexOf("m"));
-  let cat = map.indexOf(map.filter((s, i) => s.includes("C"))[0]);
-  let mouse = map.indexOf(map.filter((s, i) => s.includes("m"))[0]);
-  let indexC = locationC[cat];
-  let indexM = locationM[mouse];
-  let CatMoves = Math.abs(indexM - indexC) + Math.abs(cat - mouse);
+function catMouse(map, moves) {
+  map.split('\n');
+  const locationC = map.map((s) => s.indexOf('C'));
+  const locationM = map.map((s) => s.indexOf('m'));
+  const cat = map.indexOf(map.filter((s, i) => s.includes('C'))[0]);
+  const mouse = map.indexOf(map.filter((s, i) => s.includes('m'))[0]);
+  const indexC = locationC[cat];
+  const indexM = locationM[mouse];
+  const CatMoves = Math.abs(indexM - indexC) + Math.abs(cat - mouse);
   if (mouse === -1 || cat === -1) {
-    return "boring without two animals";
+    return 'boring without two animals';
   }
-  return CatMoves > moves ? "Escaped!" : "Caught!"; 
+  return CatMoves > moves ? 'Escaped!' : 'Caught!';
 }
 
 // task 15 https://www.codewars.com/kata/duplicate-encoder
 function duplicateEncode(word) {
-  let result = "";
-  let duplicateStr = word.toLowerCase();
+  let result = '';
+  const duplicateStr = word.toLowerCase();
   for (let i = 0; i < duplicateStr.length; i++) {
     if (duplicateStr.indexOf(duplicateStr[i]) === duplicateStr.lastIndexOf(duplicateStr[i])) {
-      result += "(";
+      result += '(';
     } else {
-      result += ")";
+      result += ')';
     }
   }
   return result;
@@ -174,38 +178,40 @@ function duplicateEncode(word) {
 
 // task 17 https://www.codewars.com/kata/576757b1df89ecf5bd00073b
 function towerBuilder(nFloors) {
-  let space, star, tower = [];
-  for(i = 1; i <= nFloors; i++){
-    space = " ".repeat(nFloors - i);
-    star  = "*".repeat((2*i) - 1);
+  let space = [];
+  let star = [];
+  const tower = [];
+  for (let i = 1; i <= nFloors; i++) {
+    space = ' '.repeat(nFloors - i);
+    star = '*'.repeat((2 * i) - 1);
     tower.push(`${space}${star}${space}`);
   }
   return tower;
 }
 
 // task 18 https://www.codewars.com/kata/58f5c63f1e26ecda7e000029
-function wave(str){
-  let arr = str.toLowerCase().split('');
- 	let result = [];
- 	for(let i = 0; i < arr.length; i++) {
- 	let temp = str.substring(0,i) + arr[i].toUpperCase() + str.substring(i+1);
- 	  if(temp != str){result.push(temp);}
- 	}
- 	return result;
+function wave(str) {
+  const arr = str.toLowerCase().split('');
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    const temp = str.substring(0,i) + arr[i].toUpperCase() + str.substring(i + 1);
+    if (temp !== str) { result.push(temp); }
+  }
+  return result;
 }
 
 // task 19 https://www.codewars.com/kata/59d398bb86a6fdf100000031
-function stringBreakers(n, string){
-  let result = [];
-  string = string.replace(/\s/g, "");
+function stringBreakers(n, string) {
+  const result = [];
+  string.replace(/\s/g, '');
   for (let i = 0; i < string.length; i += n) {
     result.push(string.substr(i, n));
   }
-  return result.join("\n");
+  return result.join('\n');
 }
 
 // task 20 https://www.codewars.com/kata/514a024011ea4fb54200004b
-function domainName(url){
-  let domainName = url.replace('http://', '').replace('https://', '').replace('www.', '').split('.')[0];
-  return domainName;
+function domainName(url) {
+  const domName = url.replace('http://', '').replace('https://', '').replace('www.', '').split('.')[0];
+  return domName;
 }
