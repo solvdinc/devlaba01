7caf6d7f4d74e0ee39ebe12f4616ffe4096b84c9
Darya-sha <70708813+Darya-sha@users.noreply.github.com>
2021-04-08 21:04:53 +0300

    fix code style

--- homeworks/darya-shaikova/06-js-basic/script.js
+++ homeworks/darya-shaikova/06-js-basic/script.js
@@ -8,13 +8,13 @@ function opposite(number) {
 
 function basicOp(operation, value1, value2) {
   if (operation === '+') {
-      return value1 + value2;
+    return value1 + value2;
   } else if (operation === '-') {
-      return value1 - value2;
+    return value1 - value2;
   } else if (operation === '*') {
-      return value1 * value2;
+    return value1 * value2;
   } else if (operation === '/') {
-      return value1 / value2;
+    return value1 / value2;
   }
 }
 
@@ -26,11 +26,11 @@ function printArray(array) {
 // task 4 https://www.codewars.com/kata/transportation-on-vacation
 function rentalCarCost(d) {
   if (d < 3) {
-      return d * 40;
+    return d * 40;
   } else if (d >= 3 && d < 7) {
-      return d * 40 - 20;
+    return d * 40 - 20;
   } else if (d >= 7) {
-      return d * 40 - 50;
+    return d * 40 - 50;
   }
 }
 
@@ -38,7 +38,7 @@ function rentalCarCost(d) {
 
 function calculation(number, operation) {
   if (!operation) {
-      return number;
+    return number;
   }
   return operation(number);
 }
@@ -64,36 +64,28 @@ function eight(operation) { return calculation(8, operation); }
 function nine(operation) { return calculation(9, operation); }
 
 function plus(x) {
-  return function(y) {
-      return y + x;
-  };
+  return (y) => y + x;
 }
 
 function minus(x) {
-  return function(y) {
-      return y - x;
-  };
+  return (y) => y - x;
 }
 
 function times(x) {
-  return function(y) {
-      return y * x;
-  };
+  return (y) => y * x;
 }
 
 function dividedBy(x) {
-  return function(y) {
-      return Math.floor(y / x);
-  };
+  return (y) => Math.floor(y / x);
 }
 
 // task 6 https://www.codewars.com/kata/get-the-middle-character
 
 function getMiddle(s) {
   if (s.length % 2 === 1) {
-      return s.substring(s.length / 2, s.length / 2 + 1);
+    return s.substring(s.length / 2, s.length / 2 + 1);
   } else if (s.length % 2 === 0) {
-      return s.substring(s.length / 2 - 1, s.length / 2 + 1);
+    return s.substring(s.length / 2 - 1, s.length / 2 + 1);
   }
 }
 
@@ -101,17 +93,17 @@ function getMiddle(s) {
 
 function partitionOn(pred, items) {
   const truth = items.filter(pred);
-  const wrong = items.filter(function(a) { return !pred(a) });
+  const wrong = items.filter((a) => !pred(a));
   items.length = 0;
   items.push.apply(items, wrong.concat(truth));
   return wrong.length;
-}
+  }
 
 // task 8 https://www.codewars.com/kata/word-count
 
 function countWords(str) {
   function isOk(value) {
-      return value;
+    return value;
   }
   return str.split(/\s/).filter(isOk).length;
 }
@@ -119,28 +111,28 @@ function countWords(str) {
 // task 9 https://www.codewars.com/kata/find-the-odd-int/
 
 function findOdd(A) {
-  let result
-  let count = 0;
-  A.sort((a, b) => a - b);
-  for (let i = 0; i < A.length; i++) {
+    let result
+    let count = 0;
+    A.sort((a, b) => a - b);
+    for (let i = 0; i < A.length; i+=1) {
       if (A[i] === A[i + 1]) {
-          count++;
+        count+=1;
       } else {
-          count++;
-          if (count % 2 === 1) {
-              result = A[i];
-              break;
-          }
+        count+=1;
+        if (count % 2 === 1) {
+          result = A[i];
+          break;
+        }
       }
+    }
+    return result;
   }
-  return result;
-}
 
 // task 10 https://www.codewars.com/kata/find-the-parity-outlier
 
 function findOutlier(integers) {
-  let even = integers.filter(i => i % 2 === 0);
-  let odd = integers.filter(i => i % 2 !== 0);
+  const even = integers.filter(i => i % 2 === 0);
+  const odd = integers.filter(i => i % 2 !== 0);
   return odd.length === 1 ? odd[0] : even[0];
 }
 
@@ -150,7 +142,7 @@ function zipWith(fn, a0, a1) {
   let minLength = Math.min(a0.length, a1.length);
   let result = [];
   for (let i = 0; i < minLength; i += 1) {
-      result.push(fn(a0[i], a1[i]));
+    result.push(fn(a0[i], a1[i]));
   }
 
   return result;
@@ -166,16 +158,16 @@ let returnNumbers = function(value) {
 
 function nthFibo(n) {
   if (n === 1) {
-      return 0;
+    return 0;
   } else if (n === 2) {
-      return 1;
+    return 1;
   }
   let a = 0;
   let b = 1;
   for (let i = 3; i <= n; i++) {
-      let c = a + b;
-      a = b;
-      b = c;
+    let c = a + b;
+    a = b;
+    b = c;
   }
   return b;
 }
@@ -189,17 +181,17 @@ function catMouse(map, moves) {
   let cat
   let mouse;
   for (let i = 0; i < map.length; i++) {
-      for (let j = 0; j < map[i].length; j++) {
-          if (map[i][j] == 'C') {
-              cat = [i, j]
-          }
-          if (map[i][j] == 'm') {
-              mouse = [i, j]
-          }
+    for (let j = 0; j < map[i].length; j++) {
+      if (map[i][j] == 'C') {
+        cat = [i, j]
       }
+      if (map[i][j] == 'm') {
+        mouse = [i, j]
+      }
+    }
   }
   if (cat == undefined || mouse == undefined) {
-      return 'boring without two animals';
+    return 'boring without two animals';
   }
   let necessaryMoves = Math.abs(cat[0] - mouse[0]) + Math.abs(cat[1] - mouse[1])
   return necessaryMoves <= moves ? 'Caught!' : 'Escaped!';
@@ -207,19 +199,17 @@ function catMouse(map, moves) {
 
 // task 15 https://www.codewars.com/kata/duplicate-encoder
 
-function duplicateEncode(word) {
-  let Word = word.toUpperCase()
-  let WordArray = Word.split('');
+function duplicateEncode(word) {  
+  let wordArray = word.toUpperCase().split('');
   let result = [];
 
-  for (let i = 0; i < WordArray.length; i++) {
-      if (WordArray.indexOf(WordArray[i]) === WordArray.lastIndexOf(WordArray[i])) {
-          result.push('(');
-      } else {
-          result.push(')');
-      }
+  for (let i = 0; i < wordArray.length; i++) {
+    if (wordArray.indexOf(wordArray[i]) === wordArray.lastIndexOf(wordArray[i])) {
+      result.push('(');
+    } else {
+      result.push(')');
+    }
   }
-
   return result.join('');
 };
 
@@ -229,9 +219,9 @@ function duplicateEncode(word) {
 function towerBuilder(nFloors) {
   let result = [];
   for (let i = 0; i < nFloors; i++) {
-      result.push(" ".repeat(nFloors - i - 1) +
-          "*".repeat((i * 2) + 1) +
-          " ".repeat(nFloors - i - 1));
+    result.push(" ".repeat(nFloors - i - 1) +
+      "*".repeat((i * 2) + 1) +
+      " ".repeat(nFloors - i - 1));
   }
   return result;
 }
@@ -241,11 +231,11 @@ function towerBuilder(nFloors) {
 function wave(str) {
   let result = [];
   for (let i = 0; i < str.length; i++) {
-      let text = str.split('');
-      if (text[i] !== ' ') {
-          text[i] = text[i].toUpperCase()
-          result.push(text.join(''))
-      }
+    let text = str.split('');
+    if (text[i] !== ' ') {
+      text[i] = text[i].toUpperCase()
+      result.push(text.join(''))
+    }
   }
   return result
 }
@@ -256,8 +246,8 @@ function stringBreakers(n, string) {
   let result = [];
   let newString = string.replace(/\s/g, "");
   while (newString.length) {
-      result.push(newString.slice(0, n));
-      newString = newString.slice(n);
+    result.push(newString.slice(0, n));
+    newString = newString.slice(n);
   }
   return result.join("\n");
 }
@@ -266,7 +256,7 @@ function stringBreakers(n, string) {
 
 function domainName(url) {
   return url.replace('http://', '')
-      .replace('https://', '')
-      .replace('www.', '')
-      .split('.')[0];
+    .replace('https://', '')
+    .replace('www.', '')
+    .split('.')[0];
 }
