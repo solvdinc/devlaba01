02e51edc8b0df3b8026013e990efb7ead1edd345
ilya07092000 <ischenko2012@gmail.com>
2021-04-08 17:57:45 +0300

    2 task

--- homeworks/ilya-ischenko/07-js-basic/script.js
+++ homeworks/ilya-ischenko/07-js-basic/script.js
@@ -11,3 +11,17 @@ function positiveSum(arr) {
   return sumArr;
 };
 console.log(positiveSum([1, -2, 3, 4, 5]));
+
+// task 2 https://www.codewars.com/kata/5a3e1319b6486ac96f000049
+function pairs(ar) {
+  let numPairs = 0;
+
+  for (let i = 0; i < ar.length - 1; i += 2) {
+    if (ar[i] + 1 === ar[i + 1] || ar[i] - 1 === ar[i + 1]) {
+      numPairs += 1;
+    }
+  }
+
+  return numPairs;
+};
+console.log(pairs([1, 2, 5, 8, -4, -3, 7, 6, 5]));
