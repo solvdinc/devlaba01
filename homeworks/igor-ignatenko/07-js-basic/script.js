84eac8f1d12ed34add5e8cd1f73f78ec9982add0
Ignatenko Igor <ignatenko.igor.ol@gmail.com>
2021-04-11 18:22:10 +0300

    refactor task 2

--- homeworks/igor-ignatenko/07-js-basic/script.js
+++ homeworks/igor-ignatenko/07-js-basic/script.js
@@ -15,7 +15,7 @@ function positiveSum(arr) {
 // task 2 https://www.codewars.com/kata/5a3e1319b6486ac96f000049
 
 function pairs(ar) {
-    const count = 0;
+    let count = 0;
 
     for (let i = 0; i < ar.length; i += 2) {
         if (ar[i] + 1 === ar[i + 1] || ar[i] - 1 === ar[i + 1]) {
