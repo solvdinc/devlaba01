79b9eac6a608d1e97b1c4b879490cb2263f453fb
ilya07092000 <ischenko2012@gmail.com>
2021-04-13 22:05:04 +0300

    3 task

--- homeworks/ilya-ischenko/08-js-part-3/script.js
+++ homeworks/ilya-ischenko/08-js-part-3/script.js
@@ -36,7 +36,7 @@ console.log(pluck(nullValue, 'unknown.key'));
 
 // task 2
 function clone(obj) {
-  const cloneObj = {};
+  let cloneObj = {};
 
   // eslint-disable-next-line no-restricted-syntax
   for (let [key, value] of Object.entries(obj)) {
@@ -54,3 +54,40 @@ clonedUser.preferences.sound.maxValue = 70;
 console.log(
   user.preferences.sound.maxValue === clonedUser.preferences.sound.maxValue,
 );
+
+// task 3
+function dateOffset(date) {
+  let result = '';
+  let secondDate = Date.parse(new Date());
+  let firstDate = date.split(',').map(n => +n.trim());
+  firstDate = Date.parse(new Date(...firstDate));
+
+  let diff = (secondDate - firstDate) / 1000;
+
+  // DAYS
+  if (diff >= 86400) {
+    const d = Math.floor(diff / 86400);
+    diff -= d * 86400;
+
+    result += d > 1 ? d + ' days ' : d + ' day ';
+  }
+
+  // HOURS
+  if (diff >= 3600) {
+    const h = Math.floor(diff / 3600);
+    diff -= h * 3600; 
+
+    result += h > 1 ? h + ' hours ' : h + ' hour ';
+  }
+
+  // MINUTES
+  if (diff >= 60) {
+    const m = Math.floor(diff / 60);
+    diff -= m;
+
+    result += m > 1 ? m + ' minutes ' : m + ' minute ';
+  }
+
+  return result += 'ago';
+};
+console.log(dateOffset('2021, 03, 5, 20, 32'));
