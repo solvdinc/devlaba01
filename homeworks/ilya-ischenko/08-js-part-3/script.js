f0213aa9a81cda81b4628e0aaf8a36a54508959a
ilya07092000 <ischenko2012@gmail.com>
2021-04-13 16:32:04 +0300

    2 task

--- homeworks/ilya-ischenko/08-js-part-3/script.js
+++ homeworks/ilya-ischenko/08-js-part-3/script.js
@@ -33,3 +33,24 @@ console.log(pluck(user, 'preferences.sound.value'));
 console.log(pluck(user, 'unknown.key'));
 console.log(pluck(randomValue, 'unknown.key'));
 console.log(pluck(nullValue, 'unknown.key'));
+
+// task 2
+function clone(obj) {
+  const cloneObj = {};
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (let [key, value] of Object.entries(obj)) {
+    if (value instanceof Object && value !== null) {
+      cloneObj[key] = clone(obj[key]);
+    } else {
+      cloneObj[key] = value;
+    }
+  }
+
+  return cloneObj;
+};
+const clonedUser = clone(user);
+clonedUser.preferences.sound.maxValue = 70;
+console.log(
+  user.preferences.sound.maxValue === clonedUser.preferences.sound.maxValue,
+);
