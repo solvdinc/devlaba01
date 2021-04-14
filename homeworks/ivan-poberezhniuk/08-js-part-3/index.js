c0c9c0ed6d6a348ab05e999ff1b01d50fd8375ba
Ivan Poberejnuk <vanyapoberejnuk@gmail.com>
2021-04-15 02:07:15 +0300

    feat: tasks 3, 4, 5, 6

--- homeworks/ivan-poberezhniuk/08-js-part-3/index.js
+++ homeworks/ivan-poberezhniuk/08-js-part-3/index.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 // Task 1 https://github.com/qaprosoft/devlaba01/blob/master/lectures/08-js-part-3/task.md#1-pluck
 const user = {
   username: 'testuser1',
@@ -75,9 +77,75 @@ console.log(user2.preferences.sound.maxValue === clonedUser1.preferences.sound.m
 console.log(user2.preferences.sound.maxValue === clonedUser2.preferences.sound.maxValue); // false
 
 // Task 3 https://github.com/qaprosoft/devlaba01/blob/master/lectures/08-js-part-3/task.md#3-a-long-time-ago
+moment.updateLocale('en', {
+  relativeTime: {
+    h: '1 hour',
+    d: '1 day',
+  },
+});
+
+const offset = (inputTime) => {
+  const currentTime = moment('23/02/2021 14:00:00', 'DD/MM/YYYY hh:mm:ss');
+  const duration = moment.duration(inputTime.diff(currentTime));
+
+  return duration.humanize(true, { m: 60, h: 24, d: 111111 });
+};
+
+// //E.g. Today is 23.02.2021, 14:00:00
+console.log(offset(moment('23/02/2021 13:30:00', 'DD/MM/YYYY hh:mm:ss')));
+// 30 minutes ago
+
+console.log(offset(moment('23/02/2021 13:00:00', 'DD/MM/YYYY hh:mm:ss')));
+// 1 hour ago
+
+console.log(offset(moment('23/02/2021 11:30:00', 'DD/MM/YYYY hh:mm:ss')));
+// 2 hours 30 minutes ago
+
+console.log(offset(moment('22/02/2021 14:00:00', 'DD/MM/YYYY hh:mm:ss')));
+// 1 day ago
+
+console.log(offset(moment('23/02/2020 10:00:00', 'DD/MM/YYYY hh:mm:ss')));
+// 366 days ago
 
 // Task 4 https://github.com/qaprosoft/devlaba01/blob/master/lectures/08-js-part-3/task.md#4-random-dates
+const date1 = moment('23/01/2021', 'DD/MM/YYYY');
+const date2 = moment('23/02/2021', 'DD/MM/YYYY');
+
+const randomDate = (from, to) => {
+  return moment(from + Math.random() * (to - from));
+};
+
+console.log(randomDate(date1, date2).format('DD/MM/YY'));
+
+// 20/02/2021
 
 // Task 5 https://www.codewars.com/kata/merged-objects
+const objConcat = (arr) => {
+  let result = {};
+  arr.forEach((obj) => {
+    result = { ...result, ...obj };
+  });
+
+  return result;
+};
 
 // Task 6 https://www.codewars.com/kata/547f1a8d4a437abdf800055c
+class NamedOne {
+  // -- SHOULD be changed --
+  constructor(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  set fullName(value) {
+    const [firstName, lastName] = value.split(' ');
+
+    if (!firstName || !lastName) return;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
