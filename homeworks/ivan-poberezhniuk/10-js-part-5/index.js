7bb928d64977006b9c297b5bcb5165d88678b8ca
Ivan Poberejnuk <vanyapoberejnuk@gmail.com>
2021-04-23 13:03:01 +0300

    fix: codestyle

---
+++ homeworks/ivan-poberezhniuk/10-js-part-5/index.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-unused-vars */
+// Task 1 https://www.codewars.com/kata/55e7650c8d894146be000095/javascript
+const validateMessage = (msg) => {
+  if (msg === null) {
+    throw ReferenceError('Message is null!');
+  }
+  if (typeof msg !== 'string') {
+    throw TypeError(`Message should be of type string but was of type ${typeof msg}!`);
+  }
+  if (msg.length === 0) {
+    throw RangeError('Message contains 0 characters!');
+  }
+  if (msg.length >= 264) {
+    throw RangeError(`Message contains ${msg.length} characters!`);
+  }
+  if (msg.includes('<') && msg.includes('>')) {
+    return false;
+  }
+
+  return true;
+};
+
+// Task 2 https://github.com/qaprosoft/devlaba01/blob/master/lectures/10-js-part-5/task.md#3-settimeoutsetinterval
+const sayJoke = async (apiUrl, jokeId) => {
+  const mixins = {
+    saySetup() {
+      return this.setup;
+    },
+    sayPunchLine() {
+      return this.punchLine;
+    },
+  };
+
+  const response = await fetch(apiUrl).then((res) => res.json());
+  if (!response.jokes) {
+    throw new Error(`No jokes at url: ${apiUrl}`);
+  }
+
+  const searchedJoke = response.jokes.filter((joke) => joke.id === jokeId)[0];
+  if (!searchedJoke) {
+    throw new Error(`No jokes found id: ${jokeId}`);
+  }
+
+  return { ...mixins, ...searchedJoke };
+};
+
+// Task 3 setTimeout/setInterval
+// https://github.com/qaprosoft/devlaba01/blob/master/lectures/10-js-part-5/task.md#3-settimeoutsetinterval
+let sec = 0;
+``;
+const timer = setInterval(() => {
+  sec += 1;
+  if (sec >= 5) {
+    clearInterval(timer);
+  }
+  console.log(`Elapsed time: ${sec} sec`);
+}, 1000);
+
+// Task 6 Digit or not
+// https://github.com/qaprosoft/devlaba01/blob/master/lectures/10-js-part-5/task.md#6-digit-or-not
+const digitOrNot = (str) => !!str.match(/^\d+/);
+
+console.log('Digit. Should be true:', digitOrNot('2021')); // true
+console.log('Word. Should be false:', digitOrNot('solvd')); // false
+console.log('Combined, starts with \\d. Should be true:', digitOrNot('2021solvd')); // true
+console.log('Combined, starts with \\w. Should be false:', digitOrNot('solvd2021')); // false
+
+// Task 7 Check if this entry is a phone number, e.g. set the format of your country:
+// https://github.com/qaprosoft/devlaba01/blob/master/lectures/10-js-part-5/task.md#7-optional-advanced
+
+const checkPhoneNUmber = (phoneNumber) => {
+  const regex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s./0-9]*$/g;
+  return !!phoneNumber.match(regex);
+};
+
+console.log(checkPhoneNUmber('+380 2222222222'));
+console.log(checkPhoneNUmber('+380 222aas   ;;sldp2222asd222'));
+console.log(checkPhoneNUmber('1123+380 2222askdmas 222222'));
+console.log(checkPhoneNUmber('as++++23da3+s+375 xx-xxx-xx-xx'));
+console.log(checkPhoneNUmber('+38(099)-75-39-91'));
---
+++ homeworks/ivan-poberezhniuk/10-js-part-5/task_5/index.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-undef */
+
+// Task 5 Fetch API/XMLHttpRequest
+const URL = 'https://randomuser.me/api/?gender=female&results=10';
+
+// Fetch request
+const getUsersWithFetch = async (cb) => {
+  try {
+    const response = await fetch(URL).then((res) => res.json());
+    cb(response.results);
+  } catch (error) {
+    console.error(`Something went wrong... ${err.message}`);
+  }
+};
+
+// XmlHttpRequest
+const getUsersWithXmlHttpRequest = async (cb) => {
+  const xhr = new XMLHttpRequest();
+  try {
+    xhr.open('GET', URL);
+    xhr.send();
+    xhr.onload = () => {
+      const response = JSON.parse(xhr.response);
+      cb(response.results);
+    };
+  } catch (err) {
+    console.error(`Something went wrong... ${err.message}`);
+  }
+};
+
+const drowUsers = async (users) => {
+  const container = document.getElementById('container');
+
+  users.forEach((person) => {
+    const article = document.createElement('article');
+    const h2 = document.createElement('h2');
+    const img = document.createElement('img');
+
+    h2.textContent = `${person.name.first} ${person.name.last}`;
+    img.src = person.picture.medium;
+    article.className = 'card';
+
+    article.append(img);
+    article.append(h2);
+
+    container.append(article);
+  });
+};
+
+getUsersWithFetch(drowUsers);
+getUsersWithXmlHttpRequest(drowUsers);
