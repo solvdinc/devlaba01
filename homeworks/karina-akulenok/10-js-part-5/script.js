66c15c8527e4352a906a6cd9b15626d5615c5d5b
Freshaku <52348428+Freshaku@users.noreply.github.com>
2021-04-24 17:20:08 +0300

    fix isues

--- homeworks/karina-akulenok/10-js-part-5/script.js
+++ homeworks/karina-akulenok/10-js-part-5/script.js
@@ -11,7 +11,8 @@ function validateMessage(msg) {
   }
   if ((msg.search('<') !== -1) && (msg.search('>') !== -1)) {
     return false;
-  } return true;
+  }
+  return true;
 }
 console.log(validateMessage());
 
@@ -20,17 +21,21 @@ function sayJoke(apiUrl, jokeId) {
   return fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
+      const { jokes } = data;
+
+      if (!jokes) {
+        return Promise.reject(new Error(`No jokes at url: ${apiUrl}`));
+      }
+
       const joke = data.jokes.find((item) => item.id === jokeId);
+
       if (joke) {
         return {
           saySetup: () => joke.setup,
           sayPunchLine: () => joke.punchLine,
         };
       }
-      throw new Error(`No jokes found id: ${jokeId}`);
-    })
-    .catch((error) => {
-      throw error instanceof TypeError ? new Error(`No jokes at url: ${apiUrl}`) : error;
+      return Promise.reject(new Error(`No jokes found id: ${jokeId}`));
     });
 }
 console.log(sayJoke());
@@ -38,14 +43,18 @@ console.log(sayJoke());
 // task 3
 function timer() {
   let seconds = 0;
-  setInterval(() => {
-    if (seconds < 5) {
-      seconds += 1;
+
+  const id = setInterval(() => {
+    seconds += 1;
+
+    if (seconds === 6) {
+      clearInterval(id);
+    } else {
       console.log(`Elapsed time: ${seconds} sec`);
-    }clearInterval();
+    }
   }, 1000);
 }
-console.log(timer());
+timer();
 
 // task 6 Digit or not
 function isRegx(str) {
@@ -63,8 +72,10 @@ console.log('will true:', isRegx('123hi'));
 
 // task 7 Optional (advanced)
 function isPhoneNumber(str) {
-  if ((str.match(/\d{3}/)[0] === '375') || (str.match(/\d{3}/)[0] === '380')) {
-    return 'It\'s a right number';
+  if ((/^\+?(\d{3})[ ](\d{2})[.-](\d{3})[.-](\d{2})[.-](\d{2})/g).test(str) || (/^\+?(\d{3})[ ](\d{2})[.-](\d{3})[.-](\d{4})/g).test(str)) {
+    if ((str.match(/\d{3}/)[0] === '375') || (str.match(/\d{3}/)[0] === '380')) {
+      return 'It\'s a right number';
+    }
   }
   return 'This number doesn\'t match';
 }
@@ -72,3 +83,5 @@ function isPhoneNumber(str) {
 console.log('will true:', isPhoneNumber('+375 23-555-44-44'));
 console.log('will true:', isPhoneNumber('+380 55-666-7777'));
 console.log('will false:', isPhoneNumber('213521252121'));
+console.log('will false:', isPhoneNumber('375546985465468498746548/987'));
+console.log('will false:', isPhoneNumber('375 а это секрет'));
