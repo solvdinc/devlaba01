4a11dc21fd989d8051d158935a360076ddf90ca3
Roman Palagniuk <101salesman@gmail.com>
2021-04-24 17:30:35 +0300

    feat: finished

--- homeworks/roman-palagniuk/11-js-part-6/11index.js
+++ homeworks/roman-palagniuk/11-js-part-6/11index.js
@@ -1,12 +1,41 @@
-function handler(e) {
-  e.target.classList.add('selectedCell');
-  console.log(e);
-  console.log(e.path[1]);
-  e.path[1].classList.add('selectedParent');
-  // e.target.parentNodeElement.classList.add('selectedParent');
-  e.target.innerHTML = `${e.target.classList[1]}`;
-  console.log(e.target);
+function createTable() {
+  const myTableDiv = document.querySelector('.container');
+  const table = document.createElement('table');
+  const tableBody = document.createElement('tbody');
+  table.appendChild(tableBody);
+  for (let i = 0; i < 30; i += 1) {
+    const tr = document.createElement('tr');
+    tr.classList.add('row');
+    tableBody.appendChild(tr);
+
+    for (let j = 0; j < 20; j += 1) {
+      const td = document.createElement('td');
+      td.classList.add('col');
+      td.dataset.x = `${j + 1}`;
+      td.dataset.y = `${i + 1}`;
+      tr.appendChild(td);
+    }
+  }
+  myTableDiv.appendChild(table);
+
+  function handler1(e) {
+    e.target.classList.add('selectedCell');
+    e.target.appendChild(
+      document.createTextNode(
+        `x: ${e.target.dataset.x}, y: ${e.target.dataset.y}`,
+      ),
+    );
+    e.target.parentElement.classList.add('selectedParent');
+    const tds = document.querySelectorAll('[data-y]');
+    console.log(e.target);
+    const colm = Array.from(tds).filter(
+      (el) => el.dataset.x === e.target.dataset.x,
+    );
+    const arr = colm.filter((el) => el.classList[1] !== e.target.classList[1]);
+    arr.forEach((el) => el.classList.add('selectedParent'));
+  }
+
+  myTableDiv.addEventListener('click', handler1);
 }
 
-const table = document.querySelector('.table');
-table.addEventListener('click', handler);
+createTable();
