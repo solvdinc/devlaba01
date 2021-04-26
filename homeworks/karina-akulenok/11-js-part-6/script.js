8b767cecfccfee334879899105468e6f0382a154
Freshaku <52348428+Freshaku@users.noreply.github.com>
2021-04-26 23:58:30 +0300

    add highlightLines

--- homeworks/karina-akulenok/11-js-part-6/script.js
+++ homeworks/karina-akulenok/11-js-part-6/script.js
@@ -1,30 +1,48 @@
 function makeGrid(row, column) {
-    const grid = document.querySelector('.grid');
-    for (i = 0; i < row; i++) {
-        for (j = 0; j < column; j++) {
-            let div = document.createElement("div");
-            div.className = "grid__item";
-            div.id = ("").concat(i,"/", j);
-            grid.appendChild(div);
+  const grid = document.querySelector('.grid');
+  for (let i = 0; i < row; i += 1) {
+    for (let j = 0; j < column; j += 1) {
+      const div = document.createElement('div');
+      div.className = 'grid__item';
+      div.id = ('').concat(i, '/', j);
+      grid.appendChild(div);
+    }
+  }
+
+  const gridItems = grid.querySelectorAll('.grid__item');
+
+  const getX = (offsetLeft) => Math.floor((offsetLeft - grid.offsetLeft) / 40) + 1;
+
+  const getY = (offsetTop) => Math.floor((offsetTop - grid.offsetTop) / 40) + 1;
+
+  function highlightLines(x, y) {
+    gridItems.forEach((element) => {
+      if (getX(element.offsetLeft) === x || getY(element.offsetTop) === y) {
+        element.classList.add('_around');
+      }
+    });
+  }
+
+  function changeColor(event) {
+    if (!event.shiftKey) {
+      gridItems.forEach((item) => {
+        const theItem = item;
+        if (theItem.classList.contains('_active')) {
+          theItem.classList.remove('_active');
+          theItem.innerText = '';
         }
+      });
     }
 
-    const gridItems = grid.querySelectorAll('.grid__item');
+    const x = getX(event.target.offsetLeft);
+    const y = getY(event.target.offsetTop);
 
+    highlightLines(x, y);
 
-    function changeColor(event) {
-        if (!event.shiftKey) {
-            gridItems.forEach((item) => {
-                if (item.classList.contains('_active')) {
-                    item.classList.remove('_active');
-                    item.innerText = '';
-                }
-            });
-        }
-        event.target.classList.add('_active');
-        event.target.innerText = `${event.target.id}`;
-    }
-    grid.addEventListener('click', changeColor);
+    event.target.classList.add('_active');
+    event.target.innerText = `${event.target.id}`;
+  }
+  grid.addEventListener('click', changeColor);
 }
 
 makeGrid(30, 20);
