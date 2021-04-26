c9225c76dec4a20f29eecbdc3219e2f6cc60f339
Darya-sha <70708813+Darya-sha@users.noreply.github.com>
2021-04-26 22:08:51 +0300

    add task

---
+++ homeworks/darya-shaikova/11-js-basics/script.js
@@ -0,0 +1,54 @@
+const grid = document.querySelector('.grid');
+const columnsCount = 20;
+const rowsCount = 30;
+const cellsCount = columnsCount * rowsCount;
+
+for (let i = 0; i < cellsCount; i += 1) {
+  const cell = document.createElement('div');
+  cell.classList.add('grid__cell');
+  grid.append(cell);
+}
+
+const gridCells = document.querySelectorAll('.grid__cell');
+
+const getX = (offsetLeft) => Math.floor((offsetLeft - grid.offsetLeft) / 40) + 1;
+
+const getY = (offsetTop) => Math.floor((offsetTop - grid.offsetTop) / 40) + 1;
+
+function highlightLines(x, y) {
+  gridCells.forEach((element) => {
+    if (getX(element.offsetLeft) === x || getY(element.offsetTop) === y) {
+      element.classList.add('_line-active');
+    }
+  });
+}
+
+function clearStyles() {
+  gridCells.forEach((element) => {
+    if (element.classList.contains('_selected')) {
+      element.classList.remove('_selected');
+      element.innerHTML = '';
+    }
+
+    if (element.classList.contains('_line-active')) {
+      element.classList.remove('_line-active');
+    }
+  });
+}
+
+function selectCell(event) {
+  if (!event.shiftKey) {
+    clearStyles();
+  }
+  const x = getX(event.target.offsetLeft);
+  const y = getY(event.target.offsetTop);
+
+  highlightLines(x, y);
+
+  event.target.innerText = `x:${x}\ny:${y}`;
+  event.target.classList.add('_selected');
+}
+
+gridCells.forEach((element) => {
+  element.addEventListener('click', selectCell);
+});
