5c45e7dad501d029eefa6737b88922a4de6100b0
Darya-sha <70708813+Darya-sha@users.noreply.github.com>
2021-04-26 07:36:41 +0300

    fix code style

--- homeworks/darya-shaikova/10-js-basics/task 5/index.js
+++ homeworks/darya-shaikova/10-js-basics/task 5/index.js
@@ -1,53 +1,52 @@
 // Task 5 Fetch API/XMLHttpRequest
-
 const URL = 'https://randomuser.me/api/?gender=female&results=20';
 
 function createCards(users) {
-	const container = document.getElementById('container');
+  const container = document.getElementById('container');
 
-	users.forEach((person) => {
-		const userCard = document.createElement('div');
-		const h2 = document.createElement('h2');
-		const img = document.createElement('img');
+  users.forEach((person) => {
+    const userCard = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const img = document.createElement('img');
 
-		h2.textContent = `${person.name.last} ${person.name.first}`;
-		img.src = person.picture.large;
-		userCard.className = 'card';
+    h2.textContent = `${person.name.last} ${person.name.first}`;
+    img.src = person.picture.large;
+    userCard.className = 'card';
 
-		userCard.append(img);
-		userCard.append(h2);
-		container.append(userCard);
-	});
-};
+    userCard.append(img);
+    userCard.append(h2);
+    container.append(userCard);
+  });
+}
 
 // Fetch request
 async function getUsersFetch(url, cb) {
-	let response = await fetch(url);
+  let response = await fetch(url);
 
-	if (response.ok) {
-		response = await response.json();
-		cb(response.results);
-	}
+  if (response.ok) {
+    response = await response.json();
+    cb(response.results);
+  }
 }
 
 getUsersFetch(URL, createCards);
 
 // XmlHttpRequest
 async function getUsersXmlHttpRequest(cb) {
-	const XMLHR = new XMLHttpRequest();
+  const XMLHR = new XMLHttpRequest();
 
-	try {
-		XMLHR.open('GET', URL);
-		XMLHR.send();
-		XMLHR.onload = () => {
-			const response = JSON.parse(XMLHR.response);
-			cb(response.results);
-		};
+  try {
+    XMLHR.open('GET', URL);
+    XMLHR.send();
+    XMLHR.onload = () => {
+      const response = JSON.parse(XMLHR.response);
+      cb(response.results);
+    };
 
-		return XMLHR;
-	} catch (error) {
-		console.error('Smth went wrong, try to check URL');
-	}
-};
+    return XMLHR;
+  } catch (error) {
+    console.error('Smth went wrong, try to check URL');
+  }
+}
 
 getUsersXmlHttpRequest(createCards);
