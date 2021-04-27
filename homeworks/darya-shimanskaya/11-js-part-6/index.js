dd50385baf0fce6c9e1d9755e2322270536cda46
Darya Shimanskaya <shymanskaya161016@gmail.com>
2021-04-27 17:39:23 +0300

    Fix issues

--- homeworks/darya-shimanskaya/11-js-part-6/index.js
+++ homeworks/darya-shimanskaya/11-js-part-6/index.js
@@ -9,37 +9,37 @@ for (let i = 0; i < 30; i += 1) {
     grid.append(div);
   }
 }
-let rowArr;
-let colArr;
+
 const cells = [...grid.children];
 
 const clickedCell = ({ target }) => {
-  cells.forEach(() => {
+  if (!target.shiftKey) {
+    cells.forEach((cell) => {
+      const currentCell = cell;
+      if (cell.style.background) {
+        currentCell.style.background = '';
+        currentCell.innerText = '';
+      }
+    });
     target.style.background = '#0b5ed7';
-    target.innerHTML = `r:${target.dataset.row}\n c:${target.dataset.column}`;
-  });
-  const unclickedCell = () => {
-    target.style.background = '';
-    target.innerHTML = '';
-    target.removeEventListener('mouseout', unclickedCell);
-    target.addEventListener('mouseover', clickedCell);
-  };
-  target.removeEventListener('mouseover', clickedCell);
-  target.addEventListener('mouseout', unclickedCell);
+    target.innerText = `r:${target.dataset.row}\nc:${target.dataset.column}`;
+  }
 };
 
 const mouseoverCell = ({ target }) => {
-  rowArr = cells.filter((cell) => cell.dataset.row === target.dataset.row);
-  colArr = cells.filter((cell) => cell.dataset.column === target.dataset.column);
-  for (let i = 0; i < rowArr.length; i += 1) rowArr[i].classList.add('_hover');
-  for (let i = 0; i < colArr.length; i += 1) colArr[i].classList.add('_hover');
+  cells.forEach((cell) => {
+    if (cell.dataset.row === target.dataset.row || cell.dataset.column === target.dataset.column) {
+      cell.classList.add('_hover');
+    }
+  });
 };
 
 const mouseoutCell = () => {
-  for (let i = 0; i < rowArr.length; i += 1) rowArr[i].classList.remove('_hover');
-  for (let i = 0; i < colArr.length; i += 1) colArr[i].classList.remove('_hover');
+  cells.forEach((cell) => {
+    cell.classList.remove('_hover');
+  });
 };
 
-cells.forEach((cell) => cell.addEventListener('click', clickedCell));
-cells.forEach((cell) => cell.addEventListener('mouseover', mouseoverCell));
-cells.forEach((cell) => cell.addEventListener('mouseout', mouseoutCell));
+grid.addEventListener('click', clickedCell);
+grid.addEventListener('mouseover', mouseoverCell);
+grid.addEventListener('mouseout', mouseoutCell);
