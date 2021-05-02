55bacee644ff0883cc11b2bd6259f3cd604795c0
Roman Ivashkevich <roman.ivashkevich963@gmail.com>
2021-05-02 15:56:26 +0300

    feat: implement logic for burger menu

--- homeworks/roman-ivashkevich/14-evns-2/src/index.js
+++ homeworks/roman-ivashkevich/14-evns-2/src/index.js
@@ -2,6 +2,10 @@ import './styles.scss';
 import './media.scss';
 
 const scrollToTopBtn = document.querySelector('.btn-top');
+const burgerButton = document.querySelector('.ncv-header-burger');
+const backdrop = document.querySelector('.backdrop');
+const header = document.querySelector('.ncv-header');
+const navBarLinks = document.querySelectorAll('.navbar__link');
 
 const showBtn = () => {
   if (window.innerWidth < 600 && window.pageYOffset > 200) {
@@ -11,10 +15,44 @@ const showBtn = () => {
   }
 };
 
+const removeActiveClass = () => {
+  document.body.classList.remove('_overflow');
+  backdrop.classList.remove('_active-backdrop');
+  header.classList.remove('_active-header');
+};
+
+const makeActiveLink = (event) => {
+  const { target } = event;
+  const activeLink = document.querySelectorAll('._active');
+  console.log(activeLink[0]);
+  console.log(target === activeLink[0]);
+  if (target !== activeLink[0]) {
+    activeLink[0].classList.remove('_active');
+    target.classList.add('_active');
+  }
+};
+
 scrollToTopBtn.addEventListener('click', () => {
   window.scrollTo(window.pageXOffset, 0);
 });
 
 document.addEventListener('scroll', () => {
   requestAnimationFrame(showBtn);
+});
+
+burgerButton.addEventListener('click', () => {
+  document.body.classList.toggle('_overflow');
+  backdrop.classList.toggle('_active-backdrop');
+  header.classList.toggle('_active-header');
+});
+
+backdrop.addEventListener('click', () => {
+  removeActiveClass();
+});
+
+navBarLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    removeActiveClass();
+    makeActiveLink(event);
+  });
 });
