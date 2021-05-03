543116f5f055c9b1efb39c6f65db87eb062f664c
Freshaku <52348428+Freshaku@users.noreply.github.com>
2021-05-04 01:12:47 +0300

    add imgs

--- homeworks/karina-akulenok/13-envs-1/gulpfile.js
+++ homeworks/karina-akulenok/13-envs-1/gulpfile.js
@@ -8,10 +8,15 @@ const gulpClean = require('gulp-clean');
 const gulpSass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const rename = require('gulp-rename');
+const changed = require('gulp-changed');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const pngquant = require('imagemin-pngquant');
 
 const paths = {
   newHtml: './src/*.html',
   styles: './src/styles/*.scss',
+  images: './src/img/**/*.{jpg,jpeg,png,gif,svg}',
 };
 
 const clean = () => src(['./dist/'], { allowEmpty: true })
@@ -25,6 +30,16 @@ const buildCss = (type = false) => src(paths.styles)
   .pipe(gulpSass({ outputStyle: type }))
   .pipe(dest('./dist/styles'));
 
+const getImages = () => src(paths.images)
+  .pipe(changed('./dist'))
+  .pipe(cache(imagemin({
+    interlaced: true,
+    progressive: true,
+    svgoPlugins: [{ removeViewBox: false }],
+    une: [pngquant()],
+  })))
+  .pipe(dest('./dist/img'));
+
 const serve = () => {
   browserSync.init({
     port: 1234,
@@ -37,16 +52,19 @@ const serve = () => {
 
   watch(paths.newHtml, series(buildHtml)).on('change', browserSync.reload);
   watch(paths.styles, series(buildCss)).on('change', browserSync.reload);
+  watch(paths.images, series(getImages)).on('change', browserSync.reload);
 };
 
 exports.build = series(
   buildHtml,
   buildCss.bind(null, 'compressed'),
+  getImages,
 );
 
 exports.start = series(
   buildHtml,
   buildCss,
+  getImages,
   serve,
 );
 
