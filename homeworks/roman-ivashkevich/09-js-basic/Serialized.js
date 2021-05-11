027a9450b12601e37559808f332ad93abf90f58b
Roman Ivashkevich <roman.ivashkevich963@gmail.com>
2021-05-12 01:46:00 +0300

    redid the task

--- homeworks/roman-ivashkevich/09-js-basic/Serialized.js
+++ homeworks/roman-ivashkevich/09-js-basic/Serialized.js
@@ -1,156 +1,284 @@
-/* eslint-disable max-classes-per-file */
-class Serializable {
-  serialize() {
-    let obj = {
-      className: `${this.constructor.name}`,
-    };
-
-    const improvedObject = Object.fromEntries(
-      Object.entries(this).map(([key, keyValue]) => {
-        let objValue = keyValue;
-
-        objValue = new Serializer(objValue).handleParser();
-
-        return [key, objValue];
-      }),
-    );
-    console.log(improvedObject);
-    obj = { ...obj, ...improvedObject };
-
-    return JSON.stringify(obj);
+class Serializer {
+  canSerialize(value) {
+    throw new Error('test');
   }
-
-  wakeFrom(serialized) {
-    try {
-      const parsedObj = JSON.parse(serialized);
-      const resObj = Object.fromEntries(
-        Object.entries(parsedObj).map(([key, value]) => {
-          let objValue = value;
-          if (key === 'className' && value !== this.constructor.name) {
-            throw new Error(
-              'Impossible to wake up from this value of serialized. Class name does not match',
-            );
-          }
-
-          objValue = new Serializer(objValue).handleParser();
-
-          return [key, objValue.value];
-        }),
-      );
-
-      return new this.constructor(resObj);
-    } catch (err) {
-      console.log(err);
-    }
+  serialize(key, value) {
+    throw new Error('test');
   }
-}
-
-class Serializer extends Serializable {
-  constructor(objValue) {
-    super();
-    this.obj =
-      typeof objValue === 'object' && !(objValue instanceof Date)
-        ? objValue
-        : { type: typeof objValue, value: objValue };
+  canWakeFrom(obj) {
+    throw new Error('test');
   }
-
-  handleParser() {
-    let res;
-    if (this.obj.type === 'string') {
-      res = new StringFieldSerializer(this.obj).handleString();
-    }
-
-    if (this.obj.type === 'number') {
-      res = new NumberFieldSerializer(this.obj).handleNumber();
-    }
-
-    if (this.obj.type === 'object' || this.obj.type === 'date') {
-      res = new ObjectFieldSerializer(this.obj).handleObject();
-    }
-
-    if (!res) {
-      throw new Error('Unexpected value');
-    }
-
-    return res;
+  wakeFrom(obj) {
+    throw new Error('test');
   }
 }
 
 class StringFieldSerializer extends Serializer {
-  constructor(objValue) {
-    super();
-    this.objValue = objValue;
+  canSerialize(value) {
+    return typeof value === 'string';
   }
 
-  handleString() {
+  serialize(key, value) {
     return {
       type: 'string',
-      value: this.objValue.value,
+      key: key,
+      value: value,
     };
+  }
+
+  canWakeFrom(obj) {
+    return obj.type === 'string';
+  }
+
+  wakeFrom(obj) {
+    return { [obj.key]: obj.value };
   }
 }
 
 class NumberFieldSerializer extends Serializer {
-  constructor(objValue) {
-    super();
-    this.objValue = objValue;
+  canSerialize(value) {
+    return typeof value === 'number' && isFinite(value);
   }
 
-  handleNumber() {
-    this.objValue.value =
-      typeof this.objValue.value === 'number' ? `${this.objValue.value}` : +this.objValue.value;
-
+  serialize(key, value) {
     return {
       type: 'number',
-      value: this.objValue.value,
+      key: key,
+      value: JSON.stringify(value),
     };
+  }
+
+  canWakeFrom(obj) {
+    return obj.type === 'number';
+  }
+
+  wakeFrom(obj) {
+    return { [obj.key]: JSON.parse(obj.value) };
+  }
+}
+
+class DateFieldSerializer extends Serializer {
+  canSerialize(value) {
+    return typeof value === 'object' && value.constructor.name === 'Date';
+  }
+
+  serialize(key, value) {
+    return {
+      type: 'date',
+      key: key,
+      value: value.toISOString(),
+    };
+  }
+
+  canWakeFrom(obj) {
+    return obj.type === 'date';
+  }
+
+  wakeFrom(obj) {
+    return { [obj.key]: new Date(obj.value) };
+  }
+}
+
+class InfinityFieldSerializer extends Serializer {
+  canSerialize(value) {
+    return typeof value === 'number' && !isFinite(value) && !isNaN(value);
+  }
+
+  serialize(key, value) {
+    return {
+      type: 'infinity',
+      key: key,
+      value: value > 0 ? '+' : '-',
+    };
+  }
+
+  canWakeFrom(obj) {
+    return obj.type === 'infinity';
+  }
+
+  wakeFrom(obj) {
+    return { [obj.key]: obj.value === '+' ? Infinity : -Infinity };
   }
 }
 
 class ObjectFieldSerializer extends Serializer {
-  constructor(objValue) {
-    super();
-    this.objValue = objValue;
+  canSerialize(value) {
+    return typeof value === 'object' && value.constructor.name === 'Object' && value !== null;
   }
 
-  handleObject() {
-    let { type } = this.objValue;
-    if (this.objValue.value instanceof Date) {
-      type = 'date';
-      this.objValue.value = this.objValue.value.toISOString();
-    }
-
-    if (typeof this.objValue.type === 'object') {
-      type = 'object';
-    }
-
-    if (this.objValue.type === 'date') {
-      this.objValue.value = new Date(this.objValue.value);
-    }
-
+  serialize(key, value) {
     return {
-      type: type,
-      value: this.objValue.value,
+      type: 'object',
+      key: key,
+      value: JSON.stringify(value),
     };
+  }
+
+  canWakeFrom(obj) {
+    return obj.type === 'object';
+  }
+
+  wakeFrom(obj) {
+    return { [obj.key]: JSON.parse(obj.value) };
   }
 }
 
+class ArrayFieldSerializer extends Serializer {
+  canSerialize(value) {
+    return Array.isArray(value);
+  }
+
+  serialize(key, value) {
+    return {
+      type: 'array',
+      key: key,
+      value: JSON.stringify(value),
+    };
+  }
+
+  canWakeFrom(obj) {
+    return obj.type === 'array';
+  }
+
+  wakeFrom(obj) {
+    return { [obj.key]: JSON.parse(obj.value) };
+  }
+}
+
+class NaNFieldSerializer extends Serializer {
+  canSerialize(value) {
+    return isNaN(value);
+  }
+
+  serialize(key, value) {
+    return {
+      type: 'NaN',
+      key: key,
+      value: JSON.stringify('str / 1'),
+    };
+  }
+
+  canWakeFrom(obj) {
+    return obj.type === 'NaN';
+  }
+
+  wakeFrom(obj) {
+    return { [obj.key]: +JSON.parse(obj.value) };
+  }
+}
+
+class NullFieldSerialize extends Serializer {
+  canSerialize(value) {
+    return value === null;
+  }
+
+  serialize(key, value) {
+    return {
+      type: 'null',
+      key: key,
+      value: JSON.stringify(value),
+    };
+  }
+
+  canWakeFrom(obj) {
+    return obj.type === 'null';
+  }
+
+  wakeFrom(obj) {
+    return { [obj.key]: +JSON.parse(obj.value) };
+  }
+}
+
+class UndefinedFieldSerialize extends Serializer {
+  canSerialize(value) {
+    if (value === undefined) {
+      throw new Error('Unexpected value');
+    }
+  }
+}
+
+class SymbolFieldSerialize extends Serializer {
+  canSerialize(value) {
+    if (typeof value === 'symbol' && value.constructor.name === 'Symbol') {
+      throw new Error('Unexpected value');
+    }
+  }
+}
+
+class Serializable {
+  static _serializersArr = [];
+
+  static register(...serializers) {
+    this._serializersArr.push(...serializers);
+  }
+
+  serialize() {
+    return JSON.stringify({
+      className: this.constructor.name,
+      fields: Object.entries(this).map(([key, value]) => {
+        const serialize = Serializable._serializersArr.find(({ canSerialize }) =>
+          canSerialize(value),
+        );
+        return serialize.serialize(key, value);
+      }),
+    });
+  }
+
+  wakeFrom(serialized) {
+    const obj = JSON.parse(serialized);
+    if (obj.className === this.constructor.name) {
+      return new this.constructor(
+        obj.fields.reduce((acc, field) => {
+          const serialize = Serializable._serializersArr.find(({ canWakeFrom }) =>
+            canWakeFrom(field),
+          );
+          return {
+            ...acc,
+            ...serialize.wakeFrom(field),
+          };
+        }, {}),
+      );
+    } else {
+      throw new Error(
+        'Impossible to wake up from this value of serialized. Class name does not match',
+      );
+    }
+  }
+}
+
+Serializable.register(
+  new StringFieldSerializer(),
+  new NumberFieldSerializer(),
+  new DateFieldSerializer(),
+  new InfinityFieldSerializer(),
+  new ArrayFieldSerializer(),
+  new ObjectFieldSerializer(),
+  new NaNFieldSerializer(),
+  new NullFieldSerialize(),
+  new UndefinedFieldSerialize(),
+  new SymbolFieldSerialize(),
+);
+
 class UserDTO extends Serializable {
-  constructor({ firstName, lastName, phone, birth, infNumb } = {}) {
+  constructor({ firstName, lastName, phone, birth, infNumb, test, test2 } = {}) {
     super();
 
     this.firstName = firstName;
     this.lastName = lastName;
     this.phone = phone;
     this.birth = birth;
-    this.infNumb = infNumb;
+    this.infNumb = -infNumb;
+    this.test = test;
+    this.test2 = test2;
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
   }
 
   printInfo() {
     console.log(
-      `${this.firstName[0]}. ${this.lastName} - ${this.phone}, ${this.birth.toISOString()}, ${
-        this.infNumb
-      }`,
+      `${this.firstName[0]}. ${this.lastName} - ${this.phone}, ${this.birth.toISOString()}`,
     );
   }
 }
@@ -158,62 +286,20 @@ class UserDTO extends Serializable {
 let tolik = new UserDTO({
   firstName: 'Anatoliy',
   lastName: 'Nashovich',
-  phone: 2020327,
+  phone: '2020327',
   birth: new Date('1999-01-02'),
-  infNumb: Infinity,
+  infNumb: -Infinity,
+  test: [1, 2, 3],
+  test2: NaN,
 });
 
 tolik.printInfo(); // A. Nashovich - 2020327, 1999-01-02T00:00:00.000Z
 
 let serialized = tolik.serialize();
+console.log(serialized);
 tolik = null;
 
 const resurrectedTolik = new UserDTO().wakeFrom(serialized);
-
+console.log(resurrectedTolik);
 console.log(resurrectedTolik instanceof UserDTO); // true
-console.log(resurrectedTolik.printInfo()); // A. Nashovich - 2020327, 1999-01-02T00:00:00.000Z
-
-class Post extends Serializable {
-  constructor({ content, date, author } = {}) {
-    super();
-
-    this.content = content;
-    this.date = date;
-    this.author = author;
-  }
-}
-
-console.log(new Post().wakeFrom(serialized));
-
-// for check
-// let test = new Post({
-//   content: 'test',
-//   date: new Date(),
-//   author: 'Roman Ivashkevich',
-// });
-
-// serialized = test.serialize(); // className Post
-// test = null;
-// let resurrectedTest = new Post().wakeFrom(serialized);
-// console.log(resurrectedTest instanceof Post); // true
-
-// class Test extends Serializable {
-//   constructor({ content, date, author } = {}) {
-//     super();
-
-//     this.content = content;
-//     this.date = date;
-//     this.author = author;
-//   }
-// }
-
-// let test = new Test({
-//   content: Infinity,
-//   date: NaN,
-//   author: -Infinity,
-// });
-
-// serialized = test.serialize();
-// test = null;
-// const resurrectedTest = new Test().wakeFrom(serialized);
-// console.log(resurrectedTest instanceof Test);
+console.log(resurrectedTolik.printInfo());
