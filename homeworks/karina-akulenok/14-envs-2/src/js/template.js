1e9d8a3c799679befa20812d698b723cef9c497b
Freshaku <52348428+Freshaku@users.noreply.github.com>
2021-05-14 03:58:45 +0300

    add actions on template

--- homeworks/karina-akulenok/14-envs-2/src/js/template.js
+++ homeworks/karina-akulenok/14-envs-2/src/js/template.js
@@ -1,1 +1,14 @@
 import '../styles/template.scss';
+
+const scrollBtn = document.querySelector('.scroll-button');
+
+scrollBtn.addEventListener('click', () => {
+  document.documentElement.scrollTop = 0;
+});
+
+const burgerMenu = document.querySelector('.header__burger-menu');
+const sideNav = document.querySelector('.side-nav');
+
+burgerMenu.addEventListener('click', () => {
+  sideNav.classList.toggle('side-nav_active');
+});
