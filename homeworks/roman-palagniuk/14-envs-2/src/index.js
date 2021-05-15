bb61563b45455e0ab4711ab1ee7faf73f8afd329
Roman Palagniuk <101salesman@gmail.com>
2021-05-15 13:07:21 +0300

    feat:config without templating

--- homeworks/roman-palagniuk/14-envs-2/src/index.js
+++ homeworks/roman-palagniuk/14-envs-2/src/index.js
@@ -1,3 +1,7 @@
+import './css/mobile.css';
+import './css/tab.css';
+import './css/CV.css';
+
 const b = document.querySelector('.burger__toggle');
 const sideSection = document.querySelector('.side');
 
