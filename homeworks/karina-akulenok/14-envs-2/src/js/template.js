cabdd5e5039b2f8bf8784d9c8d7c403b9d411616
Freshaku <52348428+Freshaku@users.noreply.github.com>
2021-05-15 19:37:27 +0300

    fix some styles for about page and add modal window

--- homeworks/karina-akulenok/14-envs-2/src/js/template.js
+++ homeworks/karina-akulenok/14-envs-2/src/js/template.js
@@ -12,3 +12,35 @@ const sideNav = document.querySelector('.side-nav');
 burgerMenu.addEventListener('click', () => {
   sideNav.classList.toggle('side-nav_active');
 });
+
+// advanced task
+
+const openModal = document.querySelector('.button__open-modal-window');
+const modalWindow = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__btn');
+const leftSide = document.querySelector('.left-side');
+const rightSide = document.querySelector('.right-side');
+const avatar = document.querySelector('.avatar');
+const headerMenu = document.querySelector('.header__menu');
+const headerLogo = document.querySelector('.header__logo');
+const avatarBg = document.querySelector('._bg');
+
+openModal.addEventListener('click', () => {
+  modalWindow.style.display = 'block';
+  avatarBg.style.display = 'block';
+  leftSide.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+  avatar.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+  rightSide.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+  headerMenu.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+  headerLogo.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+});
+
+closeModal.addEventListener('click', () => {
+  modalWindow.style.display = 'none';
+  avatarBg.style.display = 'none';
+  leftSide.style.backgroundColor = '#2d2d2d';
+  avatar.style.backgroundColor = '#2d2d2d';
+  rightSide.style.backgroundColor = 'rgb(247, 243, 233)';
+  headerMenu.style.backgroundColor = 'rgb(247, 243, 233)';
+  headerLogo.style.backgroundColor = 'rgb(247, 243, 233)';
+});
