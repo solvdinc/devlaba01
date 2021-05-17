d3de3b0ced887571397bdb6dedd0387c02c363cb
innasmiiun <innasmiyun@gmail.com>
2021-05-17 17:33:04 +0300

    Made changes to files.

--- homeworks/inna-smiiun/14-envs-2/webpack.common.js
+++ homeworks/inna-smiiun/14-envs-2/webpack.common.js
@@ -1,56 +1,52 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.[hash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/home.pug',
-            filename: 'index.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: "./src/about.pug",
-            filename: "about.html",
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: "./src/assets/",
-                    to: "assets",
-                },
-            ],
-        }),
+  entry: './src/index.js',
+  output: {
+    filename: 'main.[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/home.pug',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/about.pug',
+      filename: 'about.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './src/assets/',
+          to: 'assets',
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ca]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.pug$/,
+        use: ['pug-loader'],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-            {
-                test: /\.s[ca]ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.pug$/,
-                use: ['pug-loader']
-            },
-        ],
-    },
+  },
 };
