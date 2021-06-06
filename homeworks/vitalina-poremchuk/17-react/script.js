e613683bd9cdc29d021ec5322969b860a24f2032
PoremchukV <kvitka147@ukr.net>
2021-06-06 15:12:39 +0300

    fixed traffic light

--- homeworks/vitalina-poremchuk/17-react/script.js
+++ homeworks/vitalina-poremchuk/17-react/script.js
@@ -11,11 +11,13 @@ const TrafficLight = () => {
   React.useEffect(() => {
     const timer = setTimeout(() => {
       if (activatedLight === 2) {
+        setActivatedLight(0);
+      }
+      if (activatedLight === 1) {
+        setActivatedLight(2);
+      }
+      if (activatedLight === 0) {
         setActivatedLight(1);
-      } else if (activatedLight === 1) {
-        setActivatedLight(2);
-      } else if (activatedLight === 0) {
-        setActivatedLight(2);
       }
     }, 1000);
     return () => {
@@ -27,8 +29,8 @@ const TrafficLight = () => {
     <div className='trafficLight'>
       <div className='trafficLight__light'>
         <Light color='#DF4040' active={activatedLight === 0} />
-        <Light color='#E9EC6A' active={activatedLight === 2} />
-        <Light color='#04CA00' active={activatedLight === 1} />
+        <Light color='#E9EC6A' active={activatedLight === 1} />
+        <Light color='#04CA00' active={activatedLight === 2} />
       </div>
     </div>
   );
