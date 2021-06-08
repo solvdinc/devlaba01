3732169ed28fb14e2ba0d04855fff074fee03d76
Mariana Dzividzinska <45274002+DzividzinskaM@users.noreply.github.com>
2021-06-08 22:53:12 +0300

    fix: fix styles in AvatarsContainer

--- homeworks/mariana-dzividzinska/18-react-class-and-functional-components/avatar-app/src/components/AvatarsContainer/AvatarsContainer.jsx
+++ homeworks/mariana-dzividzinska/18-react-class-and-functional-components/avatar-app/src/components/AvatarsContainer/AvatarsContainer.jsx
@@ -10,7 +10,7 @@ class AvatarsContainer extends Component {
   constructor(props) {
     super(props);
 
-    this.state={
+    this.state = {
       avatars: [],
       showingAvatarCount: 0,
     };
