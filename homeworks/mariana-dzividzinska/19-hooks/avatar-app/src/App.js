c2d32a5787afc12f0e33a625fadff74096c6ae70
DzividzinskaM <marianadzividzinska04@gmail.com>
2021-06-09 16:30:49 +0300

    feat: add solved tasks

---
+++ homeworks/mariana-dzividzinska/19-hooks/avatar-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { AvatarsContainerFunctional } from './containers/AvatarsContainer/AvatarsContainer';
+
+function App() {
+  return (
+    <div className="App" >
+      <AvatarsContainerFunctional />
+    </div>
+  );
+};
+
+export default App;
