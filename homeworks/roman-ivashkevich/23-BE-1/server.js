f7fba344232d3ac6684d1e0461f731ce4a8d7b11
Roman Ivashkevich <roman.ivashkevich963@gmail.com>
2021-07-07 15:23:07 +0300

    feat: implement first server

---
+++ homeworks/roman-ivashkevich/23-BE-1/server.js
@@ -0,0 +1,67 @@
+'use strict';
+import http from 'http';
+import fileSystem from 'fs';
+import { URL } from 'url';
+
+const PORT = 5000;
+const PATHS = {
+  HTML: '/',
+  CSS: '/assets/main.css',
+  JS: '/assets/main.js',
+};
+
+http
+  .createServer(async (req, res) => {
+    const urlInfo = new URL(req.url, `http://${req.headers.host}`);
+
+    const openFile = (filePath, contentType) => {
+      fileSystem.readFile(filePath, (err, data) => {
+        try {
+          if (err) {
+            throw new Error('Can`t find path');
+          }
+          res.writeHead(200, {
+            'Content-Type': contentType,
+          });
+          res.write(data);
+          return res.end();
+        } catch (e) {
+          console.log(e);
+        }
+      });
+    };
+
+    switch (urlInfo.pathname) {
+      case PATHS.HTML: {
+        try {
+          openFile('./client/index.html', 'text/html');
+          break;
+        } catch (e) {
+          console.log(e);
+        }
+      }
+      case PATHS.CSS: {
+        try {
+          openFile('./client/assets/main.css', 'text/css');
+          break;
+        } catch (e) {
+          console.log(e);
+        }
+      }
+      case PATHS.JS: {
+        try {
+          openFile('./client/assets/main.js', 'application/javascript');
+          break;
+        } catch (e) {
+          console.log(e);
+        }
+      }
+      default: {
+        res.statusCode = 404;
+        res.end('Page not found');
+      }
+    }
+  })
+  .listen(PORT, 'localhost', () => {
+    console.log(`Server kicks off on PORT=${PORT}`);
+  });
