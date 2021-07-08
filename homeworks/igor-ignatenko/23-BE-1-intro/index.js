009ba7ce11439d7e8e300fc4c84f945a5e74e9e3
Ignatenko Igor <ignatenko.igor.ol@gmail.com>
2021-07-08 20:09:02 +0300

    fixed path and refactored code

--- homeworks/igor-ignatenko/23-BE-1-intro/index.js
+++ homeworks/igor-ignatenko/23-BE-1-intro/index.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const fs = require('fs');
-const url = require('url');
-const port = fs.readFileSync('port.env', 'utf8', (err, data) => data);
+const { URL } = require('url')
+const port = fs.readFileSync('port.env', 'utf8', (err, data) => +data);
+process.env.PORT = port;
 
 const server = http.createServer((req, res) => {
-  let parsedURL = url.parse(req.url, true);
-  let path = parsedURL.path.replace(/^\/+|\/+$/g, '');
+  let path = new URL(req.url, `http://${req.headers.host}`)
+    .pathname.replace(/^\/+|\/+$/g, '');
 
   if (path === '') {
     path = './assets/page.html'
@@ -15,20 +16,20 @@ const server = http.createServer((req, res) => {
     if (err) {
       res.writeHead(404);
       res.end('File not found');
-    } else {
-      if (path.endsWith('.css')) {
-        res.writeHead(200, { 'Content-Type': 'text/css' });
-      } else if (path.endsWith('.js')) {
-        res.writeHead(200, { 'Content-Type': 'text/javascript' });
-      } else if (path.endsWith('.html')) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-      }
-
-      res.end(data);
     }
+    if (path.endsWith('.css')) {
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+    } else if (path.endsWith('.js')) {
+      res.writeHead(200, { 'Content-Type': 'text/javascript' });
+    } else if (path.endsWith('.html')) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+    }
+
+    res.end(data);
+
   })
 })
 
-server.listen(+port || 8000, 'localhost', () => {
+server.listen(process.env.PORT || 8000, 'localhost', () => {
   console.log('Loading.....');
 })
